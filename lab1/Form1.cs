791a8c947c93275333cac0e5031715dbd9dac30a
Hordiychuk Vitaliy <vitalikokgo@gmail.com>
2021-11-07 21:26:21 +0300

    .

--- lab1/Form1.cs
+++ lab1/Form1.cs
@@ -8,26 +8,14 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace lab1
+namespace Lab1
 {
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
-            DataGridViewColumn A = new DataGridViewColumn();
-            DataGridViewColumn B = new DataGridViewColumn();
-            DataGridViewColumn C = new DataGridViewColumn();
-            DataGridViewColumn D = new DataGridViewColumn();
-            DataGridViewColumn E = new DataGridViewColumn();
-            DataGridViewColumn F = new DataGridViewColumn();
-            DataGridViewColumn G = new DataGridViewColumn();
-            DataGridViewColumn H = new DataGridViewColumn();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
-        }
     }
 }
