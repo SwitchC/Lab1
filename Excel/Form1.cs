6f2ed903efc0321cc3bdb60d7c65f87f2e6793cb
Hordiychuk Vitaliy <vitalikokgo@gmail.com>
2021-11-22 16:24:21 +0300

    .

--- Excel/Form1.cs
+++ Excel/Form1.cs
@@ -11,103 +11,6 @@ using ParserSpace;
 
 namespace Excel
 {
-    public class Cell : DataGridViewTextBoxCell
-        {
-            string val;
-            string name;
-            string exp;
-            public Cell()
-            {
-                name = "";
-                val = "";
-                exp = "";
-            }
-            public Cell(string n, string v, string e)
-            {
-                name = n;
-                val = v;
-                exp = e;
-            }
-            public string Name
-            {
-                get { return name; }
-                set { name = value; }
-            }
-            public string Value
-            {
-                get { return val; }
-                set { val = value; }
-            }
-            public string Exp
-            {
-                get { return exp; }
-                set { exp = value; }
-            }
-        }
-    public class Namer
-        {
-            public bool IsName(string s)
-            {
-                try
-                {
-                    Convert.ToDouble(s);
-                    return false;
-                }
-                catch (Exception)
-                {
-                    int lenS = s.Length;
-                    string colCord = "";
-                    int rowCord = 0;
-                    if ((s[lenS - 1] >= 'A' && s[lenS - 1] <= 'Z'))
-                    {
-                        return false;
-                    }
-                    for (int i = 1; i < lenS; i++)
-                    {
-                        if ((s[i] >= 'A' && s[i] <= 'Z') && (s[i - 1] >= '0' && s[i - 1] <= '9'))
-                        {
-                            return false;
-                        }
-
-                        if (s[i] >= 'A' && s[i] <= 'Z')
-                        {
-                            colCord += s[i];
-                        }
-                        if (s[i] >= '0' && s[i] <= '9')
-                        {
-                            rowCord += s[i] - '0';
-                            rowCord *= 10;
-                        }
-
-                    }
-                    return true;
-                }
-            }
-            public List<string> ListNameCell(Cell cell)
-            {
-                string s = cell.Exp;
-                List<string> res = new List<string>();
-                int pos = 0;
-                int lenS = s.Length;
-                while (pos < lenS)
-                {
-                    string name = "";
-                    if ((s[pos] >= '0' && s[pos] <= '9') || (s[pos] <= 'Z' && s[pos] >= 'A'))
-                    {
-                        while (pos < lenS && ((s[pos] >= '0' && s[pos] <= '9') || (s[pos] <= 'Z' && s[pos] >= 'A')))
-                        {
-                            name += s[pos];
-                            pos++;
-                        }
-                        if (IsName(name)) res.Add(name);
-                        else { }
-                    }
-                    pos++;
-                }
-                return res;
-            }
-        }
-
     public partial class Excel : Form
     {
         public const int maxColumn = 26;
@@ -495,4 +398,100 @@ namespace Excel
 
         }
     }
+    public class Cell : DataGridViewTextBoxCell
+        {
+            string val;
+            string name;
+            string exp;
+            public Cell()
+            {
+                name = "";
+                val = "";
+                exp = "";
+            }
+            public Cell(string n, string v, string e)
+            {
+                name = n;
+                val = v;
+                exp = e;
+            }
+            public string Name
+            {
+                get { return name; }
+                set { name = value; }
+            }
+            public string Value
+            {
+                get { return val; }
+                set { val = value; }
+            }
+            public string Exp
+            {
+                get { return exp; }
+                set { exp = value; }
+            }
+        }
+    public class Namer
+        {
+            public bool IsName(string s)
+            {
+                try
+                {
+                    Convert.ToDouble(s);
+                    return false;
+                }
+                catch (Exception)
+                {
+                    int lenS = s.Length;
+                    string colCord = "";
+                    int rowCord = 0;
+                    if ((s[lenS - 1] >= 'A' && s[lenS - 1] <= 'Z'))
+                    {
+                        return false;
+                    }
+                    for (int i = 1; i < lenS; i++)
+                    {
+                        if ((s[i] >= 'A' && s[i] <= 'Z') && (s[i - 1] >= '0' && s[i - 1] <= '9'))
+                        {
+                            return false;
+                        }
+
+                        if (s[i] >= 'A' && s[i] <= 'Z')
+                        {
+                            colCord += s[i];
+                        }
+                        if (s[i] >= '0' && s[i] <= '9')
+                        {
+                            rowCord += s[i] - '0';
+                            rowCord *= 10;
+                        }
+
+                    }
+                    return true;
+                }
+            }
+            public List<string> ListNameCell(Cell cell)
+            {
+                string s = cell.Exp;
+                List<string> res = new List<string>();
+                int pos = 0;
+                int lenS = s.Length;
+                while (pos < lenS)
+                {
+                    string name = "";
+                    if ((s[pos] >= '0' && s[pos] <= '9') || (s[pos] <= 'Z' && s[pos] >= 'A'))
+                    {
+                        while (pos < lenS && ((s[pos] >= '0' && s[pos] <= '9') || (s[pos] <= 'Z' && s[pos] >= 'A')))
+                        {
+                            name += s[pos];
+                            pos++;
+                        }
+                        if (IsName(name)) res.Add(name);
+                        else { }
+                    }
+                    pos++;
+                }
+                return res;
+            }
+        }
 }
